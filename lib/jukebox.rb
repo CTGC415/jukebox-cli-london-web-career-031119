c6d6d3bdd521ae8754d7b85ff97030af3275fc04
Cully Curwen <c.t.g.curwen@gmail.com>
2019-03-07 19:14:32 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -25,8 +25,7 @@ end
 def play(songs)
   puts "Please enter a song name or number:"
   choice = gets.chomp
-  if choice.integer?
-    choice = choice.to_i
+  if choice === choice.to_i
     choice -= 1
     !songs[choice].empty? ? (puts "Playing #{songs[choice]}") : (puts "Invalid input, please try again")
   else
